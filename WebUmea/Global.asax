6036546cc9fae8eef3bd719d83e031439cecf8f2
Peter Benko <Peter Benko@DESKTOP-6L6GM63>
2019-11-25 14:17:31 +0000

    Add project files.

---
+++ WebUmea/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebUmea.MvcApplication" Language="C#" %>
---
+++ WebUmea/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebUmea.Startup))]
+namespace WebUmea
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
