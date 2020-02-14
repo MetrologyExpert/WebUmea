38e36a1dbdc8381a22535b30edb3e24622598905
Peter Benko <Peter Benko@DESKTOP-6L6GM63>
2020-02-14 16:22:34 +0000

    InitialCommit

---
+++ RelationalDatabase/RelationalDatabase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RelationalDatabase.MvcApplication" Language="C#" %>
---
+++ RelationalDatabase/RelationalDatabase/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RelationalDatabase.Startup))]
+namespace RelationalDatabase
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
